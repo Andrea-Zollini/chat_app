f75350c5f810ce960fafbadf1727a599837e26ef
andrea <andrea.zollini@proton.me>
2024-04-23 17:52:53 +0200

    model + migrations

---
+++ app/Models/Conversation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id_1',
+        'user_id_2',
+        'last_message_id',
+    ];
+
+    public function lastMessage()
+    {
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
+
+    public function user1()
+    {
+        return $this->belongsTo(User::class, 'user_id_1');
+    }
+    public function user2()
+    {
+        return $this->belongsTo(User::class, 'user_id_2');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'avatar',
+        'email_verified_at',
+        'is_admin',
         'email',
         'password',
     ];
@@ -43,5 +46,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class, 'group_users');
     }
 }
